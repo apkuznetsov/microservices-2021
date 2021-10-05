bfc94c003d5b1cff47053267f685bc388e32c8b5
apkuznetsov <kouznts@ya.ru>
2021-10-05 18:56:36 +0400

    create template

---
+++ src/test/java/ssau/kuznetsov/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ssau.kuznetsov.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
