2ae2456aee0e60cd433a4320897240f92df040a7
apkuznetsov <kouznts@ya.ru>
2021-10-09 09:13:52 +0400

    refactor app

--- src/main/java/ssau/kuznetsov/microservices/App.java
+++ src/main/java/ssau/kuznetsov/microservices/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroservicesApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroservicesApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
