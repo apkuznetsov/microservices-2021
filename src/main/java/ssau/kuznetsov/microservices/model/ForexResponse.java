14c1b9f9b399c50fdb031c7ff7e587d2fc1d2dc7
apkuznetsov <kouznts@ya.ru>
2021-10-09 09:15:49 +0400

    impl forex response DTO

---
+++ src/main/java/ssau/kuznetsov/microservices/model/ForexResponse.java
@@ -0,0 +1,54 @@
+package ssau.kuznetsov.microservices.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Map;
+
+public class ForexResponse implements Serializable {
+
+    public Long timestamp;
+    public String base;
+    public Date date;
+    public Map<String, Double> rates;
+    private boolean success;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Map<String, Double> getRates() {
+        return rates;
+    }
+
+    public void setRates(Map<String, Double> rates) {
+        this.rates = rates;
+    }
+}
